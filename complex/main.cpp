3e212a6d2701d29619e4a8a0b1bfd8d02465aa6a
DawidCwiek <cwiekdawid1@gmai.com>
2018-11-07 10:32:03 +0100

    dodane testowanie

--- complex/main.cpp
+++ complex/main.cpp
@@ -2,13 +2,28 @@
 #include "complex.hpp"
 
 int main() {
-   Complex f = Complex(4, 0);
-   Complex s = Complex(3, 0);
-   Complex e = Complex(2, 0);
-   double a = 4, b = 3, c = 2;
-   f *= s *= e;
-   (a *= b) *= c;
-   std::cout<< "urojone: "  <<f << " " << s << " " << e << "\n";
-   std::cout<< "double: " <<a << " " << b << " " << c << "\n";
+   Complex a = Complex(4, 1);
+   Complex b = Complex(3, 2);
+   Complex c = Complex(2, 3);
+
+   a *= b *= c;
+   std::cout<< "urojone: "  << a << " " << b << " " << c << "\n";
+
+   Complex d = Complex(4, 1);
+   Complex e = Complex(3, 2);
+   Complex f = Complex(2, 3);
+
+   (d *= e) *= f;
+   std::cout<< "urojone: "  << d << " " << e << " " << f << "\n";
+
+   Complex x = Complex(2, 2);
+   Complex y = Complex(2, -2);
+   Complex z = Complex(-2, 2);
+   Complex i = Complex(-2, -2);
+
+   std::cout<< "Phaze: " << x.getPhaze() << "\n";
+   std::cout<< "Phaze: " << y.getPhaze() << "\n";
+   std::cout<< "Phaze: " << z.getPhaze() << "\n";
+   std::cout<< "Phaze: " << i.getPhaze() << "\n";
    return 0;
 }
